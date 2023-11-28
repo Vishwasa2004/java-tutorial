fed5b3b631c4bc63f9cd536308c4cd56ea5600ca
Vishwasa2004 <138768023+Vishwasa2004@users.noreply.github.com>
2023-11-28 17:45:57 +0530

    added index.java

---
+++ index.java
@@ -0,0 +1,10 @@
+class index {
+
+    public static void main(String[] args) {
+        int h=4+5;
+        if (h==9) {
+            System.out.println(h);
+        }
+        System.out.print(h);
+    }
+}
